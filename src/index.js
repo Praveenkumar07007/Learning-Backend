b51dc306e2381d86c77cd2743bd993f574c89382
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2024-12-14 21:11:21 +0530

    util codes added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 //require("dotenv").config({path: __dirname + "/.env"});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config(`path: "/.env"`);
 
-connectDB();
+connectDB().then(()=>{
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  });
+}).catch((error) => {
+  console.log("mongodb connection error: ", error);
+  process.exit(1);
+});
 
 
 
