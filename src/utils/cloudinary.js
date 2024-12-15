92c687dbd5616316481266ecf662dbbfc42817a7
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2024-12-15 18:42:01 +0530

    multer and cloudinary configuaration done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,48 @@
+
+import fs from "fs";
+import { v2 as cloudinary } from 'cloudinary';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    // Upload an image
+
+    const uploadOnCloudinary = async (localFilePath) => {
+      try {
+        if(!localFilePath) return null;
+        // Upload the image to Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+          resource_type:"auto"
+        })//file has been uploaded successfully
+        console.log("File uploaded successfully",response.url);
+        return response;
+      } catch (error) {
+        fs.unlinkSync(localFilePath);//remove the locally saved temp file as the upload operation got failed
+        return null;
+      }
+    }
+
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+
+    console.log(optimizeUrl);
+
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+
+    console.log(autoCropUrl);
+})();
