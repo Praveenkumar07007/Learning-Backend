aad9d7cde4a71c9e25c0a2f985c0c1b647a14c46
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-01-08 23:24:53 +0530

    add user channel profile retrieval with subscriber counts and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -229,11 +229,16 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
   .json(new ApiResponse(200,{}, "Password changed successfully"))
 })
 
-const getCurrentUser = asyncHandler(async(req,res)=>{
-return res.status(200)
-.json(200,req.user,"current user fetched successfully")
-
+const getCurrentUser = asyncHandler(async(req, res) => {
+  return res
+  .status(200)
+  .json(new ApiResponse(
+      200,
+      req.user,
+      "User fetched successfully"
+  ))
 })
+
 
 const updateAccountDetails = asyncHandler (async(req,res)=>{
   const {fullName,email} = req.body
@@ -257,6 +262,20 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
   if(!avatarLocalPath){
     throw new ApiError(400, "Avatar file is required")
   }
+  // TODO: delete old image
+  const oldAvatarUrl = user.avatar;
+  console.log(oldAvatarUrl)
+  // Delete the old image from Cloudinary
+  if (oldAvatarUrl) {
+    const publicId = oldAvatarUrl.split("/").pop().split(".")[0]; // Extract public ID from URL
+    try {
+      await cloudinary.v2.uploader.destroy(publicId);
+    } catch (error) {
+      console.error("Error deleting old avatar:", error.message);
+      throw new ApiError(500, "Error deleting old avatar");
+    }
+  }
+
   const avatar = await uploadOnCloudinary(avatarLocalPath)
   if(!avatar.url){
     throw new ApiError(400, "error while uploading avatar")
@@ -295,6 +314,77 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
   .json(new ApiResponse(200,user,"CoverImage updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const {username} = req.params
+
+  if (!username?.trim()) {
+      throw new ApiError(400, "username is missing")
+  }
+
+  const channel = await User.aggregate([
+      {
+          $match: {
+              username: username?.toLowerCase()
+          }
+      },
+      {
+          $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers"
+          }
+      },
+      {
+          $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "subscriber",
+              as: "subscribedTo"
+          }
+      },
+      {
+          $addFields: {
+              subscribersCount: {
+                  $size: "$subscribers"
+              },
+              channelsSubscribedToCount: {
+                  $size: "$subscribedTo"
+              },
+              isSubscribed: {
+                  $cond: {
+                      if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                      then: true,
+                      else: false
+                  }
+              }
+          }
+      },
+      {
+          $project: {
+              fullName: 1,
+              username: 1,
+              subscribersCount: 1,
+              channelsSubscribedToCount: 1,
+              isSubscribed: 1,
+              avatar: 1,
+              coverImage: 1,
+              email: 1
+
+          }
+      }
+  ])
+
+  if (!channel?.length) {
+      throw new ApiError(404, "channel does not exists")
+  }
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+  )
+})
 
 export {
   registerUser,
@@ -305,5 +395,6 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile
 };
